2dbf390cdcfeb1cbe9cc8fb2e9bf2dc9650eb139
Scott Bukowski <scottbukowski@hotmail.com>
2025-04-18 14:05:40 +0100

    Restart as 3D project.

--- src/BonzoFX.js
+++ src/BonzoFX.js
@@ -1,21 +1,14 @@
-/**
- * The global BonzoFX variable.
- */
-const BonxoFX = new BfxEngine(true);
+main();
 
-/**
- * The main engine loop.
- * @param {number} timeStamp - The timestamp from requestAnimationFrame
- */
-function engineLoop(timeStamp) {
-    const timeValue = (timeStamp - BonxoFX.timeStart);
+function main() {
+    const _canvas = document.querySelector('canvas');
+    const _gl = _canvas.getContext('webgl2');
+    if (!_gl) {
+        console.error('WebGL 2 not supported');
+        alert('WebGL 2 not supported');
+        return;
+    }
 
-    BonxoFX.update(timeValue);
-    BonxoFX.render();
-    requestAnimationFrame((t) => engineLoop(t));
+    _gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    _gl.clear(_gl.COLOR_BUFFER_BIT);
 }
-
-/**
- * Starts the main engine loop.
- */
-requestAnimationFrame(engineLoop);
